d337a66741a9707d5d0e9639e5a3e4d6fbe79e55
aman2288 <114515219+aman2288@users.noreply.github.com>
2024-08-29 18:51:11 +0530

    Update 3rd.py

--- 3rd.py
+++ 3rd.py
@@ -1,6 +1,5 @@
-# 5. Define a function that computes the length of a given list or string.
-#  (It is true that Python has the len()function built in,
-#  but writing it yourself is nevertheless a good exercise.)
+# 3.Define a function that computes the length of a given list or string.
+#  (It is true that Python has the len()function built in, but writing it yourself is nevertheless a good exercise.)
 
 l = [12,32,345,54,45,"Aman",43,54]
 def len_of_list(l):
