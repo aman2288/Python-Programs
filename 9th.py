d797a417973ae0b9fc45415e9995b57aad0b4b8a
aman2288 <114515219+aman2288@users.noreply.github.com>
2024-08-29 18:47:32 +0530

    Update 9th.py

--- 9th.py
+++ 9th.py
@@ -1,7 +1,5 @@
-# Write a function is_member()that takes a value (i.e. a number, string, etc) x and a list of values a, 
-# and returns True if x is a member of a, False otherwise.
-# (Note that this is exactly what the in operator does,
-# but for the sake of the exercise you should pretend Python did not have this operator.)
+# 9.Write a function is_member()that takes a value (i.e. a number, string, etc) x and a list of values a and returns True if x is a member of a, False otherwise.
+# (Note that this is exactly what the in operator does, but for the sake of the exercise you should pretend Python did not have this operator.)
 
 def is_member():
     a = [1, 23, 43, 54, 56, 78]
